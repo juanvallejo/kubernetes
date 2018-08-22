68515e51874f26c39575fa38db7b481844cb20be
juanvallejo <jvallejo@redhat.com>
2018-08-22 16:12:18 -0400

    simplify entrypoint

--- staging/src/k8s.io/sample-cli-plugin/cmd/kubectl-ns.go
+++ staging/src/k8s.io/sample-cli-plugin/cmd/kubectl-ns.go
@@ -9,20 +9,12 @@ import (
 	"k8s.io/sample-cli-plugin/pkg/cmd"
 )
 
-type Executable interface {
-	Execute() error
-}
-
-func newRootCommand() Executable {
-	return cmd.NewCmdNamespace(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
-}
-
 func main() {
 	flags := pflag.NewFlagSet("kubectl-ns", pflag.ExitOnError)
 	pflag.CommandLine = flags
 
-	runnable := newRootCommand()
-	if err := runnable.Execute(); err != nil {
+	root := cmd.NewCmdNamespace(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
+	if err := root.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
